42732e070212cfc856a921124ed372f970953d10
Sami Islam <islam.samipkr@gmail.com>
2023-03-29 18:23:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
 #readme
+#CREATE A FIRESTORE NATIVE DATABASE
+resource "google_firestore_database""database" {
+  project="avian-amulet-378416"
+  name="my-firestore-database"
+  location_id="US"
+  type="FIRESTORE_NATIVE"
+}
