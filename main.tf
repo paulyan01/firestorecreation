dc52e1d5c26b28080f5be19d004557ddf7e9b6c2
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-03-31 18:33:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "google"{}
 resource "google_firestore_database""database" {
   project="test-10-12345"
   name="firebase-terraform"
-  location_id="US"
+  location_id="nam5"
   type="FIRESTORE_NATIVE"
 }
