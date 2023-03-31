caa2cb54a18d1cbce77088212eefe6a0656af0d7
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-03-31 18:58:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 #CREATE A FIRESTORE NATIVE DATABASE
 provider "google"{}
 resource "google_firestore_database""database" {
-  project="test-11-12345"
+  project="test-01-12345"
   name="firebase-terraform1"
   location_id="nam5"
   type="FIRESTORE_NATIVE"
